a43c213453e813c3315cc321556ee51a5fcdbb39
rewsage <rewsage@gmail.com>
2022-08-05 09:32:46 +0800

    Redo DailyForecast component

--- src/common/components/WeekNavigation/DailyForecast/DailyForecast.jsx
+++ src/common/components/WeekNavigation/DailyForecast/DailyForecast.jsx
@@ -1,27 +1,53 @@
 import DateDisplay from "../DateDisplay/DateDisplay.jsx";
 import WeatherIcon from "../../WeatherIcon/WeatherIcon.jsx";
-import { useDispatch } from "react-redux";
+import { useSelector, useDispatch } from "react-redux";
 import { changeDay } from "Features/selectedDaySlice";
 import styles from "./DailyForecast.scss";
 
 function DailyForecast({ dailyForecastData, dayNum }) {
-	const { temp, icon, datetime } = dailyForecastData;
+	const { temp, icon, datetime, conditions, humidity } = dailyForecastData;
 	const dispatch = useDispatch();
 
+	const selectedDayNum = useSelector((state) => state.selectedDay.number);
+	const isDaySelected = dayNum === selectedDayNum;
+
+	const containerClassName = `${styles.container} ${isDaySelected ? styles.selected : ""}`;
+	const contentClassName = `${styles.content} ${isDaySelected ? styles.selected : ""}`;
+
 	return (
-		<div className={styles.container} onClick={() => dispatch(changeDay(dayNum))}>
-			<div className={styles.title}>
-				<DateDisplay datetime={datetime} />
+		<div className={containerClassName} onClick={() => dispatch(changeDay(dayNum))}>
+			<div className={contentClassName}>
+				<div className={styles.title}>
+					<DateDisplay datetime={datetime} showFullDate={isDaySelected} />
+				</div>
+
+				{isDaySelected && (
+					<div className={styles.iconWrapper}>
+						<WeatherIcon iconName={icon} />
+					</div>
+				)}
 			</div>
 
-			<div className={styles.forecastInfo}>
-				<div className={styles.iconWrapper}>
-					<WeatherIcon iconName={icon} />
-				</div>
+			<div className={contentClassName}>
+				{isDaySelected ? (
+					<div className={styles.title}>
+						<p>{conditions}</p>
+					</div>
+				) : (
+					<div className={styles.iconWrapper}>
+						<WeatherIcon iconName={icon} />
+					</div>
+				)}
 
 				<div className={styles.temp}>
 					<p>{Math.round(temp)}°</p>
 				</div>
+
+				{isDaySelected && (
+					<div className={styles.humidity}>
+						<p>Humidity {Math.round(humidity)}%</p>
+					</div>
+				)}
 			</div>
 		</div>
 	);
--- src/common/components/WeekNavigation/WeeklyNavigationBar/WeeklySummaryBar.jsx
+++ src/common/components/WeekNavigation/WeeklyNavigationBar/WeeklySummaryBar.jsx
@@ -1,33 +1,35 @@
 import DailyForecast from "../DailyForecast/DailyForecast.jsx";
 import { useSelector } from "react-redux";
 import styles from "./WeeklySummaryBar.scss";
-import DetailedDailyForecast from "../DetailedDailyForecast/DetailedDailyForecast.jsx";
+// import DetailedDailyForecast from "../DetailedDailyForecast/DetailedDailyForecast.jsx";
 
 function WeeklySummaryBar() {
 	const forecastData = useSelector((state) => state.forecastData.data);
-	const selectedDayNum = useSelector((state) => state.selectedDay.number);
+	// const selectedDayNum = useSelector((state) => state.selectedDay.number);
 	const weekDays = [0, 1, 2, 3, 4];
+
+	// const selectedDayNum = -1;
 
 	const dailyForecastList = weekDays.map((dayNum) => {
 		const dailyForecastData = forecastData.days[dayNum];
 		const dayId = dailyForecastData.datetime;
 
-		if (dayNum === selectedDayNum) {
-			return (
-				<DetailedDailyForecast
-					dailyForecastData={dailyForecastData}
-					key={dayId}
-				/>
-			);
-		} else {
-			return (
-				<DailyForecast
-					dailyForecastData={dailyForecastData}
-					dayNum={dayNum}
-					key={dayId}
-				/>
-			);
-		}
+		// if (dayNum === selectedDayNum) {
+		// 	return (
+		// 		<DetailedDailyForecast
+		// 			dailyForecastData={dailyForecastData}
+		// 			key={dayId}
+		// 		/>
+		// 	);
+		// } else {
+		return (
+			<DailyForecast
+				dailyForecastData={dailyForecastData}
+				dayNum={dayNum}
+				key={dayId}
+			/>
+		);
+		// }
 	});
 
 	return <div className={styles.wrapper}>{dailyForecastList}</div>;
